19571d6529bbbf0f3ec5cc21360d6e325af6d52a
HasanKrc0 <krchasan@icloud.com>
2025-05-12 21:23:34 +0300

    Update crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-# Cron servisini başlat
 service cron start
 
-# Yedekleme betiğinin yolu
 BACKUP_PATH="/app/backup.sh"
 
-# Yedekleme betiğine çalıştırma izni ver
-chmod +x "$BACKUP_PATH"
+CRON_JOB="@weekly $BACKUP_PATH"
 
-# Haftalık yedekleme için cron görevi
-CRON_JOB="0 0 * * 0 $BACKUP_PATH >> /var/log/cron.log 2>&1"
-
-# Log dizinini oluştur
-mkdir -p /app/backup_logs
-touch /var/log/backup.log
-chmod 666 /var/log/backup.log
-
-# Cron görevini ekle
-if crontab -l 2>/dev/null | grep -q "$BACKUP_PATH"; then
-    echo "Cron görevi zaten mevcut."
+if crontab -l 2>/dev/null | grep -q "$CRON_JOB"; then
+:
 else
     (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
-    echo "Cron görevi başarıyla eklendi: $CRON_JOB"
+    echo "$CRON_JOB"
+    echo "Cron job başarıyla eklendi:"
 fi
 
-# Flask uygulamasını başlat
 exec flask run --host=0.0.0.0
